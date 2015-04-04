0b919ec2ba962985c96aa87e5420cb2a662ecaac
Jeremy Durham <jdurham@twitter.com>
2015-04-04 10:01:01 -0400

    Use ULL2NUM and NUM2ULL instead of casting

--- ext/varint/varint.c
+++ ext/varint/varint.c
@@ -6,7 +6,7 @@ static ID getbyte, putbyte;
 static VALUE varint_encode(VALUE module, VALUE io, VALUE int_valV)
 {
     /* unsigned for the bit shifting ops */
-    unsigned long long int_val = (unsigned long long)NUM2LL(int_valV);
+    unsigned long long int_val = NUM2ULL(int_valV);
     unsigned char byte;
     while (1) {
         byte = int_val & 0x7f;
@@ -34,8 +34,7 @@ static VALUE varint_decode(VALUE module, VALUE io)
         int_val |= ((unsigned long long)(byte & 0x7f)) << shift;
         shift += 7;
         if ((byte & 0x80) == 0) {
-            /* return ULL2NUM(int_val); */
-            return LL2NUM((long long)int_val);
+            return ULL2NUM(int_val);
         }
     }
 }
